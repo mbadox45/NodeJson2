91285d0d6e92e5762d5aa151f2f66f34dca228a8
Rio Teguh A <mbadox45@gmail.com>
2023-07-28 22:19:18 +0700

    first commit 2

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.status(200).json({code:200, status:true, msg: "Hello World"});
 });
+
+
+// Read data from the JSON file
+const readData = () => {
+    const rawData = fs.readFileSync('./json/book.json');
+    return JSON.parse(rawData);
+};
+  
+// Write data to the JSON file
+// const writeData = (data) => {
+//     fs.writeFileSync('./json/book.json', JSON.stringify(data, null, 2));
+// };
+  
+// Get all books
+app.get('/books', (req, res) => {
+    const data = readData();
+    res.status(200).json({status:true,code:200,data:data.books});
+});
+  
 
 app.listen(5000, () => {
     console.log('App running di port 5000');
