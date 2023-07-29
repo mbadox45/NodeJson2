7d2dce0b345d9161051be18913c6d1507e82270b
Rio Teguh A <mbadox45@gmail.com>
2023-07-29 21:04:39 +0700

    res.json change to res.send

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 
 // Read data from the JSON file
-const readData = () => {
-    const rawData = fs.readFileSync('./data/book.json');
-    return JSON.parse(rawData);
-};
+// const readData = () => {
+//     const rawData = fs.readFileSync('./data/book.json');
+//     return JSON.parse(rawData);
+// };
 
 app.get('/', (req, res) => {
     res.status(200).send({code:200, status: true, msg: "Hello"});
@@ -20,13 +20,21 @@ app.get('/test', (req, res) => {
     res.status(200).send({code:200, status: true, msg: "Test"});
 });
 
-app.get('/book', (req, res) => {
-    try {
-        const data = readData();
-        res.status(200).json({status:true,code:200,data:data.books});
-    } catch (error) {
-        res.status(500).json({status:false,code:500,msg:"Error"});
-    }
+app.get('/user', (req, res) => {
+    fs.readFile('data/users.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error reading file:', err);
+          return res.status(500).send({ error: 'Server Error' });
+        }
+    
+        try {
+            const users = JSON.parse(data);
+            res.status(200).send(users);
+        } catch (err) {
+            console.error('Error parsing JSON:', err);
+            res.status(500).send({ error: 'Server Error' });
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
