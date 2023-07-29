cc246d9c972903c5aa5aa31efaf9bf6baca32f41
Rio Teguh A <mbadox45@gmail.com>
2023-07-29 20:48:00 +0700

    res.json change to res.send

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const usersRouter = require('./routes/users');
 
@@ -20,8 +21,12 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/book', (req, res) => {
-    const data = readData();
-    res.status(200).json({status:true,code:200,data:data.books});
+    try {
+        const data = readData();
+        res.status(200).json({status:true,code:200,data:data.books});
+    } catch (error) {
+        res.status(500).json({status:false,code:500,msg:"Error"});
+    }
 });
 
 const PORT = process.env.PORT || 3000;
