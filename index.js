3ce1b33b8c5a8993b5f04eb4d02b359a6aa626ea
Rio Teguh A <mbadox45@gmail.com>
2023-07-29 21:13:06 +0700

    res.json change to res.send

--- index.js
+++ index.js
@@ -1,46 +1,79 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
-const usersRouter = require('./routes/users');
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use('/api/users', usersRouter);
+app.get('/api/users', (req, res) => {
+  fs.readFile('data/users.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).json({ error: 'Server Error' });
+    }
 
-// Read data from the JSON file
-// const readData = () => {
-//     const rawData = fs.readFileSync('./data/book.json');
-//     return JSON.parse(rawData);
-// };
-
-app.get('/', (req, res) => {
-    res.status(200).send({code:200, status: true, msg: "Hello"});
+    try {
+      const users = JSON.parse(data);
+      res.json(users);
+    } catch (err) {
+      console.error('Error parsing JSON:', err);
+      res.status(500).json({ error: 'Server Error' });
+    }
+  });
 });
 
-app.get('/test', (req, res) => {
-    res.status(200).send({code:200, status: true, msg: "Test"});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get('/user', (req, res) => {
-    fs.readFile('data/users.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error reading file:', err);
-          return res.status(500).send({ error: 'Server Error' });
-        }
+
+
+
+
+
+
+// const express = require('express');
+// const fs = require('fs');
+// const app = express();
+// const usersRouter = require('./routes/users');
+
+// app.use(express.json());
+// app.use('/api/users', usersRouter);
+
+// // Read data from the JSON file
+// // const readData = () => {
+// //     const rawData = fs.readFileSync('./data/book.json');
+// //     return JSON.parse(rawData);
+// // };
+
+// app.get('/', (req, res) => {
+//     res.status(200).send({code:200, status: true, msg: "Hello"});
+// });
+
+// app.get('/test', (req, res) => {
+//     res.status(200).send({code:200, status: true, msg: "Test"});
+// });
+
+// app.get('/user', (req, res) => {
+//     fs.readFile('data/users.json', 'utf8', (err, data) => {
+//         if (err) {
+//           console.error('Error reading file:', err);
+//           return res.status(500).send({ error: 'Server Error' });
+//         }
     
-        try {
-            const users = JSON.parse(data);
-            res.status(200).send(users);
-        } catch (err) {
-            console.error('Error parsing JSON:', err);
-            res.status(500).send({ error: 'Server Error' });
-        }
-    });
-});
+//         try {
+//             const users = JSON.parse(data);
+//             res.status(200).send(users);
+//         } catch (err) {
+//             console.error('Error parsing JSON:', err);
+//             res.status(500).send({ error: 'Server Error' });
+//         }
+//     });
+// });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
-module.exports = app;
+// module.exports = app;
