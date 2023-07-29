0cead2de403837b0a90fc71033c0db3b5e6fb666
Rio Teguh A <mbadox45@gmail.com>
2023-07-29 20:31:38 +0700

    res.json change to res.send

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const usersRouter = require('./routes/users');
 app.use(express.json());
 app.use('/api/users', usersRouter);
 
+app.get('/', (req, res) => {
+    res.status(200).send({code:200, status: true, msg: "Hello"});
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
