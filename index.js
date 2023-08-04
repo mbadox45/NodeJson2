16a6ccb1c6274ec49bfcc0b1596eebdc6f4476db
Rio Teguh A <mbadox45@gmail.com>
2023-08-04 15:24:38 +0700

    fix

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ app.use('/api/users', usersRouter);
 app.get('/', (req, res) => {
     const filePath = path.join(process.cwd(), 'data', 'users.json');
     console.log(filePath);
-    res.status(200).send({code:200, status: true, data: filePath});
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error reading file:', err);
+          return res.status(500).json({ code: 500, status: false, message: 'Internal server error' });
+        }
+    
+        try {
+          const jsonData = JSON.parse(data);
+          return res.status(200).json({ code: 200, status: true, data: jsonData });
+        } catch (parseErr) {
+          console.error('Error parsing JSON:', parseErr);
+          return res.status(500).json({ code: 500, status: false, message: 'Error parsing JSON' });
+        }
+    });
 });
 
 app.get('/test', (req, res) => {
