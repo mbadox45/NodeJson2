9e20c50f7b628432527037ca05c56a0acbcb73af
Rio Teguh A <mbadox45@gmail.com>
2023-08-22 06:54:26 +0700

    route user by id

--- controllers/c_user.js
+++ controllers/c_user.js
@@ -56,4 +56,30 @@ c_user.get('/:id', (req, res) => {
     });
 });
 
+c_user.post('/', (req, res) => {
+    const body =  req.body;
+    console.log(filePath);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            const datas = { code: 500, status: false, message: 'Internal server error' }; 
+            return res.status(500).json(datas);
+            // return datas
+        }
+    
+        try {
+            const jsonData = JSON.parse(data);
+            const user = jsonData.find((user) => user.id === id);
+            const datas = {code:200, status:true, data:user}
+            return res.status(200).json(datas);
+            // return datas
+        } catch (parseErr) {
+            console.error('Error parsing JSON:', parseErr);
+            const datas = { code: 500, status: false, message: 'Error parsing JSON' };
+            return res.status(500).json(datas);
+            // return datas
+        }
+    });
+});
+
 module.exports = c_user;
